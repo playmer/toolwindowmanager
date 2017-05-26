4848fe8da300904e6c9d7c42fbefb42100ca6bd8
baldurk <baldurk@baldurk.org>
2017-05-26 09:52:27 +0100

    Fix string literal compile error that didn't show up before

--- example/main.cpp
+++ example/main.cpp
@@ -28,8 +28,8 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  a.setOrganizationName("ToolWindowManagerTest");
-  a.setApplicationName("ToolWindowManagerTest");
+  a.setOrganizationName(QStringLiteral("ToolWindowManagerTest"));
+  a.setApplicationName(QStringLiteral("ToolWindowManagerTest"));
   MainWindow* w = new MainWindow();
   w->show();
 
