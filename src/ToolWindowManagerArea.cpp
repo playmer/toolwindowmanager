00025b52851a8923b6a61d2a906d8aafdb8760e3
baldurk <baldurk@baldurk.org>
2017-05-29 18:20:38 +0100

    Remove area handling of mouse press/release events
    * We only want to drag from the tab title bar, dragging from other small
      spots or gaps around the area would be unintuitive.

--- src/ToolWindowManagerArea.cpp
+++ src/ToolWindowManagerArea.cpp
@@ -104,18 +104,6 @@ void ToolWindowManagerArea::updateToolWindow(QWidget* toolWindow) {
   }
 }
 
-void ToolWindowManagerArea::mousePressEvent(QMouseEvent *) {
-  if (qApp->mouseButtons() == Qt::LeftButton) {
-    m_dragCanStart = true;
-    m_dragCanStartPos = QCursor::pos();
-  }
-}
-
-void ToolWindowManagerArea::mouseReleaseEvent(QMouseEvent *) {
-  m_dragCanStart = false;
-  m_manager->updateDragPosition();
-}
-
 void ToolWindowManagerArea::mouseMoveEvent(QMouseEvent *) {
   check_mouse_move();
 }
