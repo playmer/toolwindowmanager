13451b83236c70cd6c32acca535d45f455e78cea
baldurk <baldurk@baldurk.org>
2017-05-29 18:21:18 +0100

    Stop any potential drag if we get a mouse move event with no buttons
    * This handles any edge cases where we click to start a drag (maybe)
      then something happens like a switch to another application and the
      next time we get a mouse move the button has been released.

--- src/ToolWindowManagerArea.cpp
+++ src/ToolWindowManagerArea.cpp
@@ -281,6 +281,9 @@ void ToolWindowManagerArea::restoreState(const QVariantMap &savedData) {
 }
 
 void ToolWindowManagerArea::check_mouse_move() {
+  if (qApp->mouseButtons() == Qt::LeftButton && m_dragCanStart) {
+    m_dragCanStart = false;
+  }
   m_manager->updateDragPosition();
   if (m_dragCanStart &&
       (QCursor::pos() - m_dragCanStartPos).manhattanLength() > 10) {
